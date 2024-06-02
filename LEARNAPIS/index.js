baab197406aeaf6f7d9938051573bac12c8081f7
unknown <MohammadWaseem6>
2024-06-02 09:43:17 +0530

    APIS

--- LEARNAPIS/index.js
+++ LEARNAPIS/index.js
@@ -1,13 +1,9 @@
 import express from 'express';
-
 const app = express();
 const port = 9090
-
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
-
-
 app.listen(port, () => {
     console.log(`Listening at port: ${port}`);
 });
