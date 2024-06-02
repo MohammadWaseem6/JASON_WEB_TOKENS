304809c3a03222dbce3589b4fd4c3362e3389cb7
unknown <MohammadWaseem6>
2024-06-02 11:17:24 +0530

    APIS

--- LEARNAPIS/index.js
+++ LEARNAPIS/index.js
@@ -1,9 +1,43 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
+import users from '../LEARNAPIS/MOCK_DATA.json' assert { type: 'json' };
 const app = express();
 const port = 9090
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+app.get("/users", (req, res) => {
+    const html = `
+    <ul>
+        ${users.map((user) => `<li>${user.first_name}</li>`).join('')}
+    </ul>`;
+
+    return res.send(html);
+});
+app.get("/api/users", (req, res) => {
+    return res.json(users)
+
+});
+app.get("/users/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const user = users.find((user) => user.id === id)
+    return res.json(user)
+})
+
+app.post('/api/users', (req, res) => {
+    const body = req.body;
+    return res.json({ status: "successfully data sent", body })
+
+
+
+
+
+
+
+})
 app.listen(port, () => {
-    console.log(`Listening at port: ${port}`);
+    console.log(`Listening at port: ${port} `);
 });
